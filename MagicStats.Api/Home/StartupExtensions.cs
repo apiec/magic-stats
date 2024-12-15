e86a61a922a6a3d2563967fb1c0346892d56fdab
apiec <apieczul97@gmail.com>
2024-12-15 18:51:29 +0100

    feat: add backend for some home page stats

---
+++ MagicStats.Api/Home/StartupExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace MagicStats.Api.Home;
+
+internal static class StartupExtensions
+{
+    public static IEndpointRouteBuilder AddHomeEndpoints(this IEndpointRouteBuilder app)
+    {
+        var endpoints = app
+            .MapGroup("/home")
+            .WithTags("home");
+
+        endpoints.MapEndpoint<GetHomePageStats>();
+
+        return app;
+    }
+}
