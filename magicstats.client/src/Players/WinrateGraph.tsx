abdbf98a0e24e8bb934423b882e3b1a42b867ae3
apiec <apieczul97@gmail.com>
2024-12-23 17:09:19 +0100

    feat: some winrate graph improvements

--- magicstats.client/src/Players/WinrateGraph.tsx
+++ magicstats.client/src/Players/WinrateGraph.tsx
@@ -25,25 +25,30 @@ export default function WinrateGraph({data}: WinrateGraphProps) {
 
     const maxValue = Math.max(...data.map(s => Math.max(...s.data.map(p => p.value))));
     const topValue = Math.ceil(10 * maxValue) / 10;
+    const horizontalTicks = Array.from({length: topValue / 0.2 + 1}, (_, k) => 0.2 * k);
 
     return (
-        <ResponsiveContainer width='50%' height='50%'>
+        <ResponsiveContainer width={600} height='50%'>
             <LineChart>
-                <CartesianGrid vertical={false}/>
+                <CartesianGrid vertical={false} horizontalValues={horizontalTicks} strokeWidth={1}
+                               strokeDasharray='5 5'/>
                 <XAxis
                     dataKey='date'
                     type='number'
+                    allowDuplicatedCategory={false} // without this active data point detection breaks ¯\_(ツ)_/¯
                     ticks={ticks}
                     domain={[minDate, maxDate]}
                     padding={{left: 20, right: 20}}
                     tickFormatter={(tickItem: number) => {
                         return new Date(tickItem).toLocaleDateString();
                     }}/>
-                <YAxis dataKey='value' domain={[0, topValue]}/>
-                <Tooltip/>
+                <YAxis ticks={horizontalTicks} dataKey='value' domain={[0, topValue]}
+                       tickFormatter={(tickItem: number) => tickItem.toFixed(1)}/>
+                <Tooltip formatter={(value: number, _) => (value * 100).toFixed(0)}/>
                 <Legend/>
                 {data.map((s, i) => (
-                    <Line type='monotone' dataKey='value' data={s.data} name={s.name} key={s.name} stroke={colors[i]}/>
+                    <Line type='monotone' dataKey='value' data={s.data} name={s.name} key={s.name} stroke={colors[i]}
+                          dot={false} strokeWidth={2}/>
                 ))}
             </LineChart>
 
