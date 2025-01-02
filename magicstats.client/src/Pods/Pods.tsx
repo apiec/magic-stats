9dd8be9d78a6558ea667fc148404cb9a4615c3b4
apiec <apieczul97@gmail.com>
2025-01-02 22:36:59 +0100

    feat: add pods table

--- magicstats.client/src/Pods/Pods.tsx
+++ magicstats.client/src/Pods/Pods.tsx
@@ -1,6 +1,108 @@
-﻿export default function Pods() {
+﻿import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    getSortedRowModel,
+    TableState,
+    useReactTable
+} from "@tanstack/react-table";
+import PodApi, {Pod} from "./PodApi.ts";
+import SortableHeader from "../Shared/SortableHeader.tsx";
+import {useImmer} from "use-immer";
+import {useEffect, useState} from "react";
+
+export default function Pods() {
+    const [pods, setPods] = useState<Pod[]>([])
+    useEffect(() => {
+        populatePodsData().then();
+    }, []);
+
+    async function populatePodsData() {
+        const api = new PodApi();
+        const data = await api.getAll()
+        setPods(data);
+    }
 
     return (
-        <>dupa</>
+        <section className='pods-section'>
+            <PodsTable pods={pods}/>
+        </section>
     );
 }
+type PodsTableProps = {
+    pods: Pod[],
+}
+
+function PodsTable({pods}: PodsTableProps) {
+    const table = useReactTable({
+        data: pods,
+        columns: columns,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        initialState: {
+            sorting: [
+                {
+                    id: 'size',
+                    desc: true,
+                }
+            ],
+        },
+    })
+    const [tableState, setTableState] = useImmer<TableState>({
+        ...table.initialState,
+    });
+
+    table.setOptions((prev) => {
+        return {
+            ...prev,
+            state: tableState,
+            onStateChange: setTableState,
+        };
+    });
+
+    return (
+        <table className='pods-table'>
+            <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                    {headerGroup.headers.map(header => (
+                        <th key={header.id}>
+                            {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                        </th>
+                    ))}
+                </tr>
+            ))
+            }
+            </thead>
+            <tbody>
+            {table.getRowModel().rows.map(row => (
+                <tr key={row.id}>
+                    {row.getVisibleCells().map(cell => (
+                        <td key={cell.id}>
+                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </td>))}
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+}
+
+const columnHelper = createColumnHelper<Pod>();
+
+const columns = [
+    columnHelper.accessor('size', {
+        id: 'size',
+        header: ctx => <SortableHeader text='Size' context={ctx}/>,
+        cell: ctx => <p className='size-cell'>{ctx.getValue()}</p>
+    }),
+    columnHelper.display({
+        id: 'players',
+        header: 'Players',
+        cell: ctx => <p>{ctx.row.original.players.map(p => p.name).sort().join(', ')}</p>
+    }),
+    columnHelper.accessor('games', {
+        id: 'games',
+        header: ctx => <SortableHeader text='Games' context={ctx}/>,
+    }),
+];
